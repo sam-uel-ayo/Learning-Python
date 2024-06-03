89b3b2d463b8c9f4659a963d7ddb0d326c7c8797
sam-uel-ayo <samuelayomide0705@gmail.com>
2024-06-03 21:18:14 +0100

    An interactive python session

---
+++ Chapter_1/Exercise_1.py
@@ -0,0 +1,40 @@
+#Start up an interactive Python session and try typing in each of the following commands.
+#Write down the results you see.
+    #(a) print("Hello, world!")
+    #(b) print("Hello", "world!")
+    #(c) print(3)
+    #(d) print(3.0)
+    #(e) print(2 + 3)
+    #(f) print(2.0 + 3.0)
+    #(g) print("2" + "3")
+    #(h) print("2 + 3 =", 2 + 3)
+    #(i) print(2 * 3)
+    #(j) print(2 ** 3)
+    #(k) print(2 / 3)
+    
+    
+
+print("Hello, world!")
+print("Hello", "world!")
+print(3)
+print(3.0)
+print(2 + 3)
+print(2.0 + 3.0)
+print("2" + "3")
+print("2 + 3 =", 2 + 3)
+print(2 * 3)
+print(2 ** 3)
+print(2 / 3)
+
+#Results: 
+#Hello, world!
+#Hello world!
+#3
+#3.0
+#5
+#5.0
+#23
+#2 + 3 = 5
+#6
+#8
+#0.6666666666666666
