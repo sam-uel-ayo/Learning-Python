431f1309fe8c85b80206c1d4adc771d9be387332
sam-uel-ayo <samuelayomide0705@gmail.com>
2024-06-11 21:03:34 +0100

    Quadratic function

--- Practice.py
+++ Practice.py
@@ -1,16 +1,17 @@
-# futval.py
-# A program to compute the value of an investment
-# carried 10 years into the future
+# quadratic.py
+# A program that computes the real roots of a quadratic equation.
+# Illustrates use of the math library.
+# Note: this program crashes if the equation has no real roots.
+import math # Makes the math library available.
 def main():
-    print("This program calculates the future value")
-    print("of a 10-year investment.")
-
-    principal = eval(input("Enter the initial principal: "))
-    apr = eval(input("Enter the annual interest rate: "))
+    print("This program finds the real solutions to a quadratic")
+    print()
     
-    for i in range(10):
-        principal = principal * (1 + apr)
-        
-    print("The value in 10 years is:", principal)
+    a, b, c = eval(input("Please enter the coefficients (a, b, c): "))
+    discRoot = math.sqrt(b * b - 4 * a * c)
+    root1 = (-b + discRoot) / (2 * a)
+    root2 = (-b - discRoot) / (2 * a)
     
+    print()
+    print("The solutions are:", root1, root2 )
 main()
