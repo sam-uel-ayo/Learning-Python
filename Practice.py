8ba24c5e3080fecf9360a010261717a2163ccec9
sam-uel-ayo <samuelayomide0705@gmail.com>
2024-06-13 21:55:21 +0100

    Round function

--- Practice.py
+++ Practice.py
@@ -1,20 +1,9 @@
-# factorial.py
-# Program to compute the factorial of a number
-# Illustrates for loop with an accumulator
-def main():
-    n = eval(input("Please enter a whole number: "))
-    fact = 1
-    for factor in range(n,1,-1):
-        fact = fact * factor
-    print("The factorial of", n, "is", fact)
-main()
+# Rounding.py
+# Program to round numbers 
 
-
-def main():
-    numbers = eval(input("What is number: "))
-    fact = 1
-    while numbers > 1:
-        fact = numbers * fact
-        numbers = numbers - 1
-    print("The factorial is", fact) 
-main() 
+import math
+math.pi
+round(math.pi, 2)
+round(math.pi,3)
+print(round(math.pi, 2))
+print(round(math.pi,3))
